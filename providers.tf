ef18e97040f00cb62f8737042bf540a306a984ea
M <869279@cognizant.com>
2020-12-18 20:46:00 +0530

    added providers

--- providers.tf
+++ providers.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "madhu-tf-123"
-    key    = "terraform.tfstate"
+    key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
 }
